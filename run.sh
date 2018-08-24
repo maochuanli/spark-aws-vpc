8ef7035ccddec584a5eb77f81653facbed0b96ac
mao.chuan.li <mao.chuan.li@gmail.com>
2018-08-24 18:58:06 +1200

    Support deploying the Django web application from remote github repository
    https://codeload.github.com/maochuanli/spark-aws-webapp/zip/master
    
    Signed-off-by: Mao Li <mao.chuan.li@gmail.com>

--- run.sh
+++ run.sh
@@ -68,5 +68,15 @@ if [ "$createVPC" = "true" ]; then
 elif [ "$destroyVPC" = "true" ]; then
     ansible-playbook -i "local, " -e @$SCRIPTPATH/default.vars.yml -e aws_access_key=$AWS_ACCESS_KEY -e aws_secret_key=$AWS_SECRET_KEY $SCRIPTPATH/DestroyVPC.yml $@
 elif [ "$setupVPC" = "true" ]; then
-    ansible-playbook -i $SCRIPTPATH/ec2_instance_inventory.txt -e @$SCRIPTPATH/default.vars.yml -e ansible_user=ubuntu -e ansible_ssh_private_key_file=$SCRIPTPATH/maoli_key_pair.pem SetupWeb.yml $@
+    if [ -z "$GODADDY_ACCESS_KEY" ] || [ -z "$GODADDY_SECRET_KEY" ]; then
+        echo "ERROR: "
+        echo "    \$GODADDY_ACCESS_KEY variable and/or \$GODADDY_SECRET_KEY is not set"
+        echo ""
+        exit 1
+    fi
+    ansible-playbook -i $SCRIPTPATH/ec2_instance_inventory.txt -e @$SCRIPTPATH/default.vars.yml \
+        -e ansible_user=ubuntu -e ansible_ssh_private_key_file=$SCRIPTPATH/maoli_key_pair.pem \
+        -e go_daddy_key=$GODADDY_ACCESS_KEY -e go_daddy_secret=$GODADDY_SECRET_KEY \
+        -e aws_access_key=$AWS_ACCESS_KEY -e aws_secret_key=$AWS_SECRET_KEY \
+        SetupWeb.yml $@
 fi
